a0880586a8f3e754acf616dc2e7093022f29c655
Dusty the Fuzzy Dragon <dusty@chcl.se>
2024-09-27 14:50:12 +0200

    Add full name to license header, per Certum request

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 Sebastian "Dusty the Fuzzy Dragon" Johansson
+// Copyright 2024 Tore Leo Sebastian "Dusty the Fuzzy Dragon" Johansson
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
